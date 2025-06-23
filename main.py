ef04527bc072955fd56113081469a0285b036bda
talenaa <talenabarbosa7@gmail.com>
2025-06-23 19:17:08 -0300

    feat: f strings

--- main.py
+++ main.py
@@ -7,3 +7,13 @@ print("it's cold outside")
 
 first_name = "talena"
 print(first_name) # When printing a variable don't put it in double or single quotes, this will print the literal name of the variable and not the value
+
+# f-strings - A feature that improves printing and readability. For example:
+
+second_name = "barbosa"
+age = "18"
+print(f"my second name is {second_name} and i am {age} years old") # By using an f-string we can add multiple variables, making it easier to put values.
+
+nationality = "brazilian"
+holiday = "são joão"
+print(f"i am {nationality} and my favorite brazilian holiday is {holiday}")
