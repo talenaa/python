d83e514620e5a97ec6f06838fe21de77652ea13b
talenaa <talenabarbosa7@gmail.com>
2025-06-23 18:38:50 -0300

    feat: variables

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 print("I like volleyball")
 print("it's cold outside")
+
+# Variables - A container for a value (string, integer, floater, boolean), a variable behave as if it was the value it contains 
+
+# Always remeber that the variables must have unique names
+
+first_name = "talena"
+print(first_name) # When printing a variable don't put it in double or single quotes, this will print the literal name of the variable and not the value
