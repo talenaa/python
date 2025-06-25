fce270dbbdae1f15c0a43c10515e5370cb3aabf5
talenaa <talenabarbosa7@gmail.com>
2025-06-24 21:32:20 -0300

    feat: boolean

--- main.py
+++ main.py
@@ -40,4 +40,23 @@ gpa = 4.7
 print(f"my gpa is {gpa}")
 
 distance = 10.5
-print(f"the distance until the beach is {distance}km")   
+print(f"the distance until the beach is {distance}km")
+
+# Boolean - booleans are either true or false. For example: 
+
+is_raining = True # The true or false must be in capital
+print(f"is it raining right now? {is_raining}")
+
+# With boolean values, we really don't output them directly. It's more likely to see them being used internally within a program, such as when working with if statements. For example:
+
+if is_raining:
+    print("It's not raining")
+else:
+    print("It's raining right now")
+
+for_sale = False
+
+if for_sale:
+    print("that item is not available")
+else:
+    print("that item is for sale")
